41713f55393809b4863319f3a013e7dba426d09c
Eduardo Marges <135554783+How1tzer@users.noreply.github.com>
2023-09-11 18:08:31 +0000

    Movimiento de personaje Basado en el mapa

--- jugador.js
+++ jugador.js
@@ -1,4 +1,4 @@
-/*
+
 //jugador.js
 
 //Frames de rana Default
@@ -17,12 +17,12 @@ class Jugador {
 
 const jugador1 = new Jugador(up_default, 330, 630, 35, 30);
 
-function update(ctx, jugador1) {
-    
+function update(jugador1) {
     ctx.fillStyle = jugador1.color;
     ctx.fillRect(jugador1.jx, jugador1.jy, jugador1.height, jugador1.width);
     ctx.drawImage(jugador1.outfit, jugador1.jx-31, jugador1.jy-33,100,100);
+    console.log("hola");
     
+   
 
 }
-*/
